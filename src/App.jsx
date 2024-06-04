6c20c9f2a83ff91f32493edad059f67c1578c902
Srikanth-Ganti7 <ganti.srikanth7@gmail.com>
2024-06-04 15:19:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { BrowserRouter  } from "react-router-dom";
+import { HashRouter, BrowserRouter  } from "react-router-dom";
 
 import{ About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
 
@@ -9,7 +9,8 @@ import{ About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsC
 const App = () => {
 
   return (
-    <BrowserRouter>
+    <HashRouter>
+    {/* <BrowserRouter> */}
       <div className = "relative z-0 bg-primary">
         <div className="bg-herp-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
@@ -26,7 +27,8 @@ const App = () => {
         </div>
 
         </div>
-    </BrowserRouter>
+    {/* </BrowserRouter> */}
+    </HashRouter>
   )
 }
 
