7b4ac43f78d1ad2adf21b6a1b1ea516e93ff47f4
Srikanth-Ganti7 <ganti.srikanth7@gmail.com>
2024-07-21 12:37:58 +0530

    Version 2 - updated the overview and contact 3d model

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -44,19 +44,19 @@ const About = () => {
 
       <motion.p variants = {fadeIn("","",0.1,1)} className = "mt-4 text-secondary text-[17px] max-w-3xl leading-[30px] ">
         <p>
-          I am Balasai Srikanth Ganti, an enthusiastic and highly motivated individual with a deep passion for new technologies and a relentless drive to push my boundaries. Currently, I am pursuing a Master's in Computer Science at the University of Florida, where I maintain a stellar CGPA, reflecting my commitment to academic excellence.
+        I am Balasai Srikanth Ganti, a highly motivated individual with a passion for new technologies. Currently pursuing a Master's in Computer Science at the University of Florida, I excel academically with a stellar CGPA.
         </p><br/>
         <p>
-          My technical skills span a wide range of programming languages, including Python, C++, JavaScript, Java, and F#, and I have hands-on experience with databases such as MySQL, MongoDB, and Elasticsearch. I am also proficient in web technologies like HTML, Figma, and CSS, and have a keen eye for design with expertise in Adobe Photoshop and Adobe Illustrator.
+        My skills include Python, C++, JavaScript, Java, F#, and experience with databases like MySQL, MongoDB, and Elasticsearch. I am proficient in web technologies such as HTML, Figma, and CSS, and have design expertise in Adobe Photoshop and Illustrator.
         </p><br/>
         <p>
-          Professionally, I have worked as a Project Engineer at Wipro Ltd, where I played a crucial role in improving business processes and enhancing system reliability. My experience extends to roles such as a Graphic Designer at University of Florida office of advancement, Ganges Valley School and internships at Indian Space Research Organization, where I gained valuable insights and practical experience.
+        Professionally, I have served as a Project Engineer at Wipro Ltd, enhancing business processes and system reliability. Additionally, I have experience as a Graphic Designer at the University of Florida and Ganges Valley School, along with internships at Indian Space Research Organization.
         </p><br/>
         <p>
-          Beyond my professional roles, I am dedicated to making a positive impact through volunteer work with Make A Difference, where I have contributed to educational initiatives for underprivileged children. My technical projects, ranging from developing a 3D graphics rendering application to creating a library management system and a Unity game, showcase my ability to tackle diverse challenges and innovate.
+        I am committed to volunteer work with Make A Difference, contributing to educational initiatives for underprivileged children. My projects include a 3D graphics rendering application, a library management system, and a Unity game, demonstrating my ability to tackle diverse challenges.
         </p><br/>
         <p>
-          I am always eager to embrace new technological challenges, continuously pushing myself to learn and grow in this ever-evolving field.
+        I continuously seek new technological challenges to learn and grow in this dynamic field.
         </p><br/>
       </motion.p>
       <div className='mt-20 flex flex-wrap gap-10'>
--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -6,16 +6,33 @@ import CanvasLoader from '../Loader';
 
 
 const Earth = () => {
-  const earth = useGLTF("./planet/scene.gltf")
+  const earth = useGLTF("./treecko_and_mudkip/scene.gltf")
   
   
   return (
+    <mesh>
+      <ambientLight intensity={1.25}/>
+      <hemisphereLight intensity={0.15} groundColor='black' />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={-0.32}
+        penumbra={1}
+        intensity={1000}
+        castShadow
+        // shadow-mapSize={1024}
+      />
+      /<pointLight intensity={2} />
     <primitive
       object={earth.scene}
-      scale={2.5}
+      //scale={0.002}//floating_island
+      //scale = {1}//pokeball-lowpoly
+      scale = {0.07} 
       position-y={0}
       rotation-y = {0}
     />
+    </mesh>
+
+    
 
   )
 }
@@ -31,7 +48,8 @@ const EarthCanvas = () => {
       fov:45,
       near:0.1,
       far:200,
-      position:[-4,3,6]
+      position:[-2,4,6]
+      //position:[-4,3,6]
     }}
     >
       <Suspense fallback = {<CanvasLoader/>}>
